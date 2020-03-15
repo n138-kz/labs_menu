48c91c75cc891c2e0c12a24bb24a0140ba862972
yneon@n138.nws <lent38g-net@yahoo.co.jp>
2020-03-15 19:18:22 +0900

    Add new indexfile

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: text/html');
+?><!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Labs menu</title>
+  <style media="screen">
+    li:nth-child(even){
+      background-color: azure;
+    }
+    li{
+      list-style: none;
+    }
+  </style>
+  </head>
+  <body>
+    <?php
+      foreach( glob('../*') as $key => $val ){
+        if( false ){
+        } elseif( $val == '../index.php' ){
+          continue;
+        }
+        echo '<li><a href="' . $val . '">' . $val . '</a></li>';
+      }
+    ?>
+  </body>
+</html>
